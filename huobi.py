82f5e62c91b065fe3137af84407339e0851d46dc
hua-elliott <1352133162@qq.com>
2017-12-02 23:55:08 +0800

    加入一些函数
    平仓、买入、卖出等

--- huobi.py
+++ huobi.py
@@ -15,7 +15,7 @@ import time
 
 
 
-
+#下面为QQ邮件提醒功能
 def qqsmtp(money):
     _user = "1352133162@qq.com"
     _pwd  = "lpigwyhdhtaobacc"
@@ -34,7 +34,11 @@ def qqsmtp(money):
     except smtplib.SMTPException,e:
         print "Falied,%s"%e
 
+
+
+#突破后执行的函数
 def tupo_1(real,Open,Close,High,Low,b):
+    
     '''
     if Open > Close:
         med = (Open-Close)/2.0 + Close
@@ -50,23 +54,84 @@ def tupo_1(real,Open,Close,High,Low,b):
     
     return 1
     
-def tupo_2(real,Open,Close,High,Low,b):
-    '''
-    if Open > Close:
-        med = (Open-Close)/2.0 + Close
-    else:
-        med = (Close-Open)/2.0 + Open
-    '''
+
     
-    if 1==1:
-        print u"符合条件进行向下突破!"
+def get_value(): #获取当前币价
+    while(1):
+        try:
+            r = requests.get('https://api.huobi.pro/market/detail/merged?symbol=btcusdt').text
+            n = demjson.decode(r)
+            break
+        except:
+            #time.sleep(2)
+            continue
+    
+    
+    return n['tick']['close'] # return float
+    
+    
+def get_smavalue(how_long): #获取当前SMA值  ep: how_long 必须为int型
+    while(1):
+        time.sleep(2)
+        try:
+            print "进入"
+            r = requests.get('https://api.huobi.pro/market/history/kline?symbol=btcusdt&period=5min&size=25',timeout=5).text        
+        except:
+            time.sleep(2)
+            print "出错"
+            continue
+    a = [] #数组暂存收盘价用来接下来MACD,SMA生成
         
-            
-          
-        if 1 == 1:
-            kaishi_2(b)
-                    
-    return 1
+    for i in range(1,25):
+        a.append(n['data'][i]['close']) #return float, this is close value
+        
+        
+    a.reverse() #此处翻转是为了下面的MACD及SMA生成        
+    a = np.array(a)
+        
+    real = talib.SMA(a,timeperiod=how_long) #EMA线
+    
+    return real[-2] #-2无误
+    
+
+def success_sma20():
+    
+
+def pc(shijian,b,v):#ep:  shijian 循环处理时用的时间戳,b:MACD柱子值,v下单时的价格
+    bitcoin_value = get_value() #获取价格
+    sma_value = get_smavalue(10) #获取与之对应的SMA10日均线
+    if bitcoin_value > sma_value: #先清半仓,若大于十日线则开始20日均线止盈措施
+        sell(bitcoin_value,1) #清半仓
+        success_sma20() #开始20sma止盈措施
+    else:
+        sell(bitcoin_value,2) #否则全仓卖出
+    
+    
+
+def buy(value): #买入默认全仓买入
+    global zijin
+    global bitcoin
+    
+    bitcoin_num = (zijin*0.997)/value #手续费是0.2% 即 0.002  1-0.002 = 0.998 在此我提高一点算0.997,除以value最后\
+    #得到币的数量 return float
+    
+    bitcoin = bitcoin_num #更新持币数量
+    zijin = zijin*0.003
+    return 1 #返回1表示成功买入
+    
+
+
+def sell(value,status): # ep: value:实时币价,status：1半仓卖出还是2全仓卖出
+    global zijin
+    global bitcoin
+    if status == 1:
+        zijin += bitcoin * value * 0.997 #扣除手续费
+        bitcoin = 0.0 #清空持仓
+    if status == 2:
+        zijin += (bitcoin/2.0) * value * 0.997
+        bitcoin -= bitcoin/2.0 #更新持仓
+    return 1#返回1表示卖出成功
+
 def kaishi_1(b):
     global time_1
     
@@ -81,6 +146,7 @@ def kaishi_1(b):
             continue
     
     value = n['tick']['close'] #float
+    buy(value) #执行买单
     zhisun = value*(1-0.005) #设止损为跌0.5%
     print u"下单价格:"+str(value)
     print u"向上突破第一阶段,止损价:"+str(zhisun)
@@ -113,9 +179,8 @@ def kaishi_1(b):
             a = np.array(a)
             dif,dea,bar = talib.MACD(a,fastperiod=6,slowperiod=13,signalperiod=6)
 
-            if bar[-1] < b:
-                print u"bar<0加1"
-                kk += 1
+            if bar[-1] < b:#当macd柱小于前柱时平半仓
+               pc(shijian,bar[-1],value) #进入平半仓函数
             time_1 = shijian
         #time.sleep(2)
         print u"进入22"
@@ -167,137 +232,6 @@ def kaishi_1(b):
         
     return 1
         
-def kaishi_2():
-    global zijin
-    global time_1
-    global p_1
-    global p_2
-    global qr_1
-    global qr_2
-    #time.sleep(2)
-    while(1):
-        try:
-            r = requests.get('http://120.24.241.55:61001/?query=price&type=jsonret&symbol=EURUSD').text
-            break
-        except:
-            #time.sleep(2)
-            continue
-    n = demjson.decode(r)
-    value = float(n['list'][0]['price'])
-    zhisun = value + 0.0010
-    zhiyin = value - 0.0010
-    print u"下单价格:"+str(value)
-    print u"向下突破第一阶段下单价格:"+str(value)+u"止盈价"+str(zhiyin)+u"止损价:"+str(zhisun)
-    
-    f = open(u"下单.txt","a+")
-    f.write("向下突破第一阶段下单价格:"+str(value)+"止盈价"+str(zhiyin)+"止损价:"+str(zhisun)+"\n")
-    f.close()
-    
-
-    kk = 0
-    #time.sleep(2)
-    while(1):
-        if kk >=3:
-            autopy.mouse.move(p_1,p_2)
-            autopy.mouse.click()
-            time.sleep(3)
-            autopy.mouse.move(qr_1,qr_2)
-            autopy.mouse.click()
-            while(1):
-                try:
-                    
-                    r = requests.get('http://120.24.241.55:61001/?query=price&type=jsonret&symbol=EURUSD').text
-                    n = demjson.decode(r)
-                    break
-                except:
-                    #time.sleep(2)
-                    continue
-            
-            close_1 = float(n['list'][0]['price'])
-            sss = value-close_1
-            if sss<0:
-                zijin += sss*25000
-                f = open(u"下单.txt","a+")
-                f.write("向下突破第一阶段止损完毕1价格:"+str(close_1)+"  当前资金池:"+str(zijin)+"\n\n\n\n")
-                f.close()
-                qqsmtp(zijin)
-                break
-            if sss>0:
-                zijin += sss*25000
-                f = open(u"下单.txt","a+")
-                f.write("向下突破第一阶段止赢完毕0价格:"+str(close_1)+"  当前资金池:"+str(zijin)+"\n\n\n\n")
-                f.close()
-                qqsmtp(zijin)
-                break
-        
-        #time.sleep(3)
-        print u"进入22"
-        while(1):
-            try:
-                r = requests.get('http://120.24.241.55:61002/getDataHis.ashx?type=stock&min=5&symbol=EURUSD').text
-                n = demjson.decode(r[11:-1])
-                break
-            except:
-                #time.sleep(2)
-                continue
-        shijian = n['min'][-1]
-        if shijian != time_1:
-            a = []
-            for i in range(-21,-1):
-                a.append(float(n['data'][i][3]))
-            
-            a = np.array(a)
-            dif,dea,bar = talib.MACD(a,fastperiod=6,slowperiod=13,signalperiod=6)
-            
-            if bar[-1] > 0:
-                print u"bar>0 加1"
-                kk += 1
-            time_1 = shijian
-
-        #time.sleep(2)
-        while(1):
-            try:
-                r = requests.get('http://120.24.241.55:61001/?query=price&type=jsonret&symbol=EURUSD').text
-                n = demjson.decode(r)
-                break
-            except:
-                time.sleep(2)
-                continue
-        close_1 = float(n['list'][0]['price'])
-        if close_1 <= zhiyin:
-            autopy.mouse.move(p_1,p_2)
-            autopy.mouse.click()
-            time.sleep(3)
-            autopy.mouse.move(qr_1,qr_2)
-            autopy.mouse.click()
-            print u"向下突破第一阶段止盈完毕,最低价:"+str(close_1)
-            zijin += (value-close_1)*25000
-            print u"当前资金池:"+str(zijin)
-            
-            f = open(u"下单.txt","a+")
-            f.write("向下突破第一阶段止盈完毕价格:"+str(close_1)+"  当前资金池:"+str(zijin)+"\n\n\n\n")
-            f.close()
-            qqsmtp(zijin)
-            
-            
-            break
-        if close_1 >= zhisun:
-            autopy.mouse.move(p_1,p_2)
-            autopy.mouse.click()
-            time.sleep(3)
-            autopy.mouse.move(qr_1,qr_2)
-            autopy.mouse.click()
-            print u"向下突破第一阶段止损完毕,最高价:"+str(close_1)
-            
-            zijin -= (close_1-value)*25000
-            
-            f = open(u"下单.txt","a+")
-            f.write("向下突破第一阶段止损完毕价格:"+str(close_1)+"  当前资金池为:"+str(zijin)+"\n\n\n\n")
-            f.close()
-            qqsmtp(zijin)
-            
-            break
-    return 1
 
 
 #程序入口
@@ -305,13 +239,16 @@ if __name__ == '__main__':
     
     global ss #择时中间变量
     global time_1 #择时中间变量
-    global zijin
-
-
-
-
-    zijin = 5000
+    global zijin #现金数
+    global bitcoin
+    global all_zijin #资金总值
     
+
+
+
+    all_zijin = 0
+    zijin = 5000 #出事资金5000
+    bitcoin = 0  #出事比特币数量0
     ss = 0
     time_1 = 0
     
@@ -359,6 +296,4 @@ if __name__ == '__main__':
         if bar[-2] < 0 and bar[-1] > 0:
             print "向上突破"
             pp = tupo_1(float(real[-1]),Open,Close,High,Low,bar[-1]) #向上突破
-        if bar[-2] > 0 and bar[-1] < 0:
-            print "向下突破"
-            pp = tupo_2(float(real[-1]),Open,Close,High,Low,bar[-1]) #向下突破
+
